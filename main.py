a0bf1e074dd1c00875c0bb8fd52723a0b3bbe0ea
Francis Quezon <aisukezon@gmail.com>
2025-11-08 22:08:46 +1100

    Update README.md as well as pass environment varible into inotifywait subprocess

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import os
+
 from src.logger import logger
 from src.redis import get_redis_client, send_to_redis
 from src.inotifywait_subprocess import InotifyWatcher
 
 
-WATCH_DIR = "/watched"
+WATCH_DIR = os.getenv("WATCH_DIR", "/watched")
+INCLUDE_DOTFILES = os.getenv("INCLUDE_DOTFILES", "false") == "true"
 
 
 def main():
     logger.info("Starting...")
-    watcher = InotifyWatcher(WATCH_DIR)
+    watcher = InotifyWatcher(WATCH_DIR, INCLUDE_DOTFILES)
     watcher.start()
     redis_client = get_redis_client()
 
